3623c6410ee1acce77bae87595b46d725e30c442
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-12 17:09:06 +0000

    automatically backed up by learn

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,8 +1,3 @@
-# If you write a describe block without quotes like this, you need to define
-# the described class (or method) for the tests to even run. It's sort of a
-# freebie test to make sure you've actually defined the models you plan on
-# testing.
-
 describe Person do
 
   let(:avi_attributes) {
